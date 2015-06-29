d49c2b28476fb81134334b8d1943643027983ed8
sealday <sealday@gmail.com>
2015-06-29 11:57:57 +0800

    get items from server

--- app/js/app.js
+++ app/js/app.js
@@ -3,15 +3,30 @@
  */
 var meanApp = angular.module('mean', ['ui.router', 'ngAnimate']);
 
+meanApp.factory('itemService', function($http){
+   var instance = {};
+
+    instance.items = [];
+
+    instance.refresh = function() {
+        $http.get('/items').success(function(data) {
+            console.log(data);
+           instance.items.length = 0;
+            angular.forEach(data, function(item) {
+                instance.items.push(item);
+            });
+        });
+    }
+
+    instance.refresh();
+
+    return instance;
+});
+
 meanApp.controller('HomeController', function(){
 
 });
 
-meanApp.controller('ListController', function(){
-    this.items = [
-        'They failed by only one vote',
-        'Too little, too late',
-        'Setting an example',
-        'Opposition weakness'
-    ];
+meanApp.controller('ListController', function(itemService){
+    this.items = itemService.items;
 });
