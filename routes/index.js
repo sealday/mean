10a91bdc5d45d57647b2d03d3e100483077c93fe
sealday <sealday@gmail.com>
2015-06-29 11:40:53 +0800

    add items url response

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+
+router.get('/items', function(req, res, next) {
+   res.json([
+       "Where are you?",
+       "Something wrong.",
+       "Have lunch."
+   ]);
+});
+
 module.exports = router;
